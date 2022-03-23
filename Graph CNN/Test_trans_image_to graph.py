f5f91f6f0548194620f0272129bd128c06f3c4f1
XuanSon <ahsanori06@gmail.com>
2022-03-23 15:01:31 +0700

    change

--- Graph CNN/Test_trans_image_to graph.py
+++ Graph CNN/Test_trans_image_to graph.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy.spatial import distance
 img =cv.imread("D:\Hoc_ky_212_2022\De_cuong_luan_van_tot_nghiep\gg (9).jpg",cv.IMREAD_GRAYSCALE)
-image = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,8,2)
-#image = cv.resize(img_1, (50,50))
+cat,img_1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+#image = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,8,2)
+image = cv.resize(img_1, (50,50))
 
 
 #CONSTRUCTION OF HORIZONTAL EDGES
--- Graph CNN/test.py
+++ Graph CNN/test.py
@@ -7,10 +7,8 @@ import scipy
 from scipy.spatial import distance
 
 img =cv.imread("D:\Hoc_ky_212_2022\De_cuong_luan_van_tot_nghiep\gg (9).jpg",cv.IMREAD_GRAYSCALE)
-threshold = img.copy()
-threshold[threshold <= 120] = 0
-threshold[threshold > 120] = 255
+cat,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
 #image = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
 #image = cv.resize(img_1, (50,50))
-cv.imshow("cat",img)
+cv.imshow("cat",thresh1)
 cv.waitKey()
